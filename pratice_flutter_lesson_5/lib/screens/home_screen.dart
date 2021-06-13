a66925d426926eeb646a6abedb597e79e106f735
quang10296 <duyquangnguyen10296@gmail.com>
2021-06-13 09:44:21 +0700

    [Lesson_5]_Hotfix_setting_widget

--- pratice_flutter_lesson_5/lib/screens/home_screen.dart
+++ pratice_flutter_lesson_5/lib/screens/home_screen.dart
@@ -59,11 +59,17 @@ class HomeScreen extends StatelessWidget {
                   child: ElevatedButton(
                     style: style,
                     onPressed: () => tinhDiemTrungBinh(
-                              diemToan: double.parse(_diemToanController.text),
-                              diemVan: double.parse(_diemVanController.text),
-                              diemAnh: double.parse(_diemAnhController.text),
-                            ).runtimeType ==
-                            double
+                              diemToan: _diemToanController.text.length == 0
+                                  ? -1.0
+                                  : double.parse(_diemToanController.text),
+                              diemVan: _diemVanController.text.length == 0
+                                  ? -1.0
+                                  : double.parse(_diemVanController.text),
+                              diemAnh: _diemAnhController.text.length == 0
+                                  ? -1.0
+                                  : double.parse(_diemAnhController.text),
+                            ) !=
+                            -1.0
                         ? showDialog<String>(
                             context: context,
                             builder: (BuildContext context) => AlertDialog(
@@ -100,7 +106,7 @@ class HomeScreen extends StatelessWidget {
                               ],
                             ),
                           )
-                        : alertValidateInput(),
+                        : showAlertValidateInput(context),
                     child: const Text('Check'),
                   ),
                 ),
@@ -117,15 +123,47 @@ class HomeScreen extends StatelessWidget {
       @required double diemVan,
       @required double diemAnh}) {
     double diemTrungBinh = (diemAnh + diemToan + diemVan) / 3;
-    if ((diemTrungBinh > 0) && (diemTrungBinh < 10)) {
-      return diemTrungBinh;
-    }
+    print(diemTrungBinh != -1.0);
+    return diemTrungBinh;
   }
 
-  Widget alertValidateInput() {
-    return AlertDialog(
-      title: const Text("NHẬP LỖI !!!"),
-      content: const Text('Vui lòng kiểm ra và nhậ lại số điểm !'),
+  showAlertValidateInput(BuildContext context) {
+    // set up the buttons
+    Widget cancelButton = TextButton(
+      child: Text("Cancel"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+    Widget continueButton = TextButton(
+      child: Text("Continue"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text(
+        " LỖI !!! ",
+        textAlign: TextAlign.center,
+      ),
+      content: Text(
+        "Số điểm nhập không đúng định dạng \n Vui lòng nhập đúng định dạng số điểm !",
+        textAlign: TextAlign.center,
+      ),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
     );
   }
 
